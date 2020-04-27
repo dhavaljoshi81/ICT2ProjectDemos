bdb0e0ca282a825433ad650bacc89534507f201a
dhavaljoshi81 <59249502+dhavaljoshi81@users.noreply.github.com>
2020-04-27 12:15:23 +0530

    Order Management Web API
    Order Management Web API

---
+++ OrderManagementWebAPICS/Models/ActionFilterEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrderManagementWebAPICS.Models
+{
+    public partial class ActionFilterEntry
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ OrderManagementWebAPICS/Models/OrderDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrderManagementWebAPICS.Models
+{
+    public partial class OrderDetail
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Rate { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
